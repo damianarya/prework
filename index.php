d8bdc77386789fe06a4708b2793eb9d2f156a04f
Damian Arya <dahakcyrus@yahoo.com>
2019-09-23 21:16:49 -0600

    added starter index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>I compleated the prework</title>
+	<head>
+	<body>
+		<p>I compleated the prework</p>
+	<body>
+<html>
