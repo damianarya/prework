d942cc8834113771b9b79962d8a97d9732cd25a5
Damian Arya <dahakcyrus@yahoo.com>
2020-03-08 20:45:09 -0600

    added comentary

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<title>I compleated the prework</title>
-	</head>
-	<body>
-	<p>I compleated the prework!</p>
-	</body>
+<head>
+	<meta charset="utf-8" />
+	<title>I compleated the prework</title>
+</head>
+<!-- as indicated by the title this was one of the last prework requierment to begin the class-->
+<body>
+<p>I compleated the prework!</p>
+</body>
 </html>
