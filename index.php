62f0cb0a80b7645b2fec57a28d1fa8bb6aeaa875
Damian Arya <dahakcyrus@yahoo.com>
2020-03-11 20:57:37 -0600

    modifyed line seven

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8" />
 	<title>I compleated the prework</title>
 </head>
-<!-- as indicated by the title this was one of the last prework requierment to begin the class!-->
+<!-- as indicated by the title this was one of the last prework requierments to begin the class!-->
 <body>
 <p>I compleated the prework!</p>
 </body>
